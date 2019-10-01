f25724b565e704f4b40284dd4bd30bed314eb240
17130068 <17130068@st.hcmuaf.edu.vn>
2019-10-01 23:38:51 +0700

    web ca nhan
    HCI

---
+++ 17130068_Design/sitemap.php
@@ -0,0 +1,10 @@
+<?php echo "hi"; exit; /*
+#Home|index.html|#Home|0|1|FALSE|FALSE|0|
+      #Home Page|index.html|#Home|0|1|FALSE|FALSE|0||index|<id>23|23|23|Home Page|</id>|1|ENGLISH|UTF-8||FALSE|FALSE|FALSE|EN|FALSE|0||4|FALSE|FALSE|
+#EXERCISES|exercises.html|#EXERCISES|0|203|FALSE|FALSE|0|
+      #Exercises|exercises.html|#EXERCISES|0|203|FALSE|FALSE|0||exercises|<id>24|24|24|Exercises|</id>|1|ENGLISH|UTF-8||FALSE|FALSE|FALSE|EN|FALSE|0||4|FALSE|FALSE|
+      #Lab1.html|lab1.html|#EXERCISES|0|0|FALSE|FALSE|0||lab1|<id>28|24|28|Lab1.html|</id>|1|ENGLISH|UTF-8||FALSE|FALSE|FALSE|EN|FALSE|0||4|FALSE|FALSE|
+#ABOUT|about.html|#ABOUT|0|201|FALSE|FALSE|0|
+      #About|about.html|#ABOUT|0|201|FALSE|FALSE|0||about|<id>27|27|27|About|</id>|1|ENGLISH|UTF-8||FALSE|FALSE|FALSE|EN|FALSE|0||4|FALSE|FALSE|
+
+*/ ?>
